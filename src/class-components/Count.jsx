a339b4d5505dc917721017861b13d701ee56e4e9
Kate Grigoryeva <kate23april@yandex.ru>
2022-10-17 19:31:42 +0300

    lesson 1

---
+++ src/class-components/Count.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export class Count extends React.Component {
+    state = {
+        count: 5
+    }
+
+    handleClick = () => {
+        this.setState({ count: this.state.count +1 })
+    }
+
+    render() {
+        return <>
+            <p>Count props: {this.props.count}</p>
+            <p>Count state: {this.state.count}</p>
+            <button type="button" onClick={this.handleClick}>btn</button>
+        </>
+    }
+}
---
+++ src/components/message/Message.jsx
@@ -0,0 +1,20 @@
+import style from './Message.module.css'
+
+export const Message = ({message, handleChangeMessage}) => {
+    const handleClick = () => {
+        handleChangeMessage('This message was sent from Message Component')
+    }
+
+    const handleChange = (e) => {
+        handleChangeMessage(e.target.value)
+    }
+
+    return <div className={style.message}>
+        <div>
+            <p className="parent-message">{message}</p>
+        </div>
+        {/* <button onClick={handleClick}>Send Message</button> */}
+        <h3 className={style.message_header}>Input message to Parent here:</h3>
+        <input className={style.message_input} onChange={handleChange} />
+    </div>
+} 
