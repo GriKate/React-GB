cb02fc325ffd188c64760750fc2a989e2f0987c1
Kate Grigoryeva <kate23april@yandex.ru>
2022-10-28 19:56:27 +0300

    Add routes by react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,38 +4,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// const span = React.createElement('span', {}, 'span')
-
-// const element = React.createElement('h1', {
-//   className: 'my-class'
-// }, span)
-
-
-// JSX - JS extension
-// const element = () => {
-//   return <div>my first elem</div>
-// }
-
 const messages = [
     {author: 'Ann', text:'This message was sent from Ann'},
     {author: 'Nick', text:'This is an answer from Nick'}
   ]
-// const messages = []
-// console.log(typeof messages)
 
 function addMessage(message) {
     messages.push(message)
 }
 
-const chats = [
-  {id: '1', name: 'Weather'}, 
-  {id: '2', name: 'City'}, 
-  {id: '3', name: 'Transport'}
-]
+// const chats = [
+//   {id: '1', name: 'Weather'}, 
+//   {id: '2', name: 'City'}, 
+//   {id: '3', name: 'Transport'}
+// ]
 
 // до 18 версии был метод рендер, теперь createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App messages={messages} chats={chats} addMessage={addMessage} />);
+root.render(<App messages={messages} addMessage={addMessage} />);
 
 // root.render(element());
 
